c33195821a5c4740ee899d1a3a349fa40d59938b
vivekkm251 <65061181+vivekkm251@users.noreply.github.com>
2020-05-29 08:13:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,27 +13,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',(req,res) => {
    
-const conn = new Client({
-    user: "syfbrmjtdmpwtm",
-    password :"51093b60a4b1680d8a25ba146f15c9ccb966d23320bcc3fc319e1824ee0ae4ba",
-    host : "ec2-34-230-149-169.compute-1.amazonaws.com",
-    port:5432,
-    database : "d7q778i7grjqu4"
-})
+// const conn = new Client({
+//     user: "syfbrmjtdmpwtm",
+//     password :"51093b60a4b1680d8a25ba146f15c9ccb966d23320bcc3fc319e1824ee0ae4ba",
+//     host : "ec2-34-230-149-169.compute-1.amazonaws.com",
+//     port:5432,
+//     database : "d7q778i7grjqu4"
+// })
 
-conn.connect()
-    .then(() => console.log("successful"))
-    .then(() => conn.query("select * from user123"))
-    .then(results => console.table(results.rows))
-    .then(() => conn.query('INSERT INTO user123(id, name) VALUES ($1,$2)',[8,'Amitabh Bacchan']))
-    .then(() => conn.query("select * from user123"))
-    .then(results => console.table(results.rows))
-    .then(() => conn.query('DELETE FROM user123 WHERE id = $1',[8]))
-    .then(() => conn.query("select * from user123"))
-    // .then(results => console.table(results.rows))
-    .then(results => res.send(results.rows))
-    .catch(e => console.log(e))
-    .finally(() => {conn.end()})
+// conn.connect()
+//     .then(() => console.log("successful"))
+//     .then(() => conn.query("select * from user123"))
+//     .then(results => console.table(results.rows))
+//     .then(() => conn.query('INSERT INTO user123(id, name) VALUES ($1,$2)',[8,'Amitabh Bacchan']))
+//     .then(() => conn.query("select * from user123"))
+//     .then(results => console.table(results.rows))
+//     .then(() => conn.query('DELETE FROM user123 WHERE id = $1',[8]))
+//     .then(() => conn.query("select * from user123"))
+//     // .then(results => console.table(results.rows))
+//     .then(results => res.send(results.rows))
+//     .catch(e => console.log(e))
+//     .finally(() => {conn.end()})
+   
+   res.send("hello world!");
 });
 
 
