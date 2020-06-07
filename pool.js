425ff334fce46c7aaa940cc33f104b35b5a819ce
vivekkm251 <kalamtechcorp@gmail.com>
2020-06-07 17:48:36 +0530

    Registration and Login Functionality implemented

--- pool.js
+++ pool.js
@@ -1,10 +1,10 @@
 var pg = require('pg')
-var PGUSER = 'postgres'
-var PGDATABASE = 'TravelPoints1'
+var PGUSER = 'wqrew'
+var PGDATABASE = 'd7q778i7grjqu4'
 var config = {
   user: PGUSER, // name of the user account
   database: PGDATABASE, // name of the database
-  password :"a",
+  password :"51093b60a4b1680d8a25ba146f15c9ccb966d23320bcc3fc319e1824ee0ae4ba",
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000 
 }
