b4b66b60450355824e0f56bbc24d78e6bb206ed6
vivekkm251 <kalamtechcorp@gmail.com>
2020-06-07 17:56:56 +0530

    Registration and Login Functionality implemented

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const postController = require('../model/registerUser');
 const loginController = require('../model/loginUser');
 
 const router = express.Router();
+router.get('/',res.send("hello world"));
 router.post('/register',postController.registerUser);
 router.post('/login',loginController.loginUser);
 
