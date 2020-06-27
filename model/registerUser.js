ec7e5c122044f39ebc28a469be8c8a1313a1c895
vivekkm251 <kalamtechcorp@gmail.com>
2020-06-27 15:58:02 +0530

    Hashmap Implementation

--- model/registerUser.js
+++ model/registerUser.js
@@ -4,7 +4,7 @@ let validateKey = require('../AESEncrypt')
 
 const registerUser = (req,res) => {
 var token = req.headers.validateToken;
-console.timeLog(token);
+console.log(token);
 var salt = token.split("_");
 // 'fHX4xp6L9IE1YUfCLknn9w==','DcV0VRYa3D48MhvAWj1qtxpHt6TtSbcNAbXX5UfhrSg='
 
