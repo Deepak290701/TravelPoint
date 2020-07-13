03f4b40ae593dad663990252ea39e150fb5f73e8
vivekkm251 <kalamtechcorp@gmail.com>
2020-07-13 12:44:17 +0530

    Random ID generation

--- model/registerUser.js
+++ model/registerUser.js
@@ -12,36 +12,36 @@ class registerUser {
    async registerUser1 (req,res) {
 
 
-        // var token =  req.get('validateToken');
-        // if(!(token.length==69)){
+        var token =  req.get('validateToken');
+        if(!(token.length==69)){
         
-        //     return  res.json({
-        //         posts : [
-        //             {statusCode: "1005"},
-        //                     {message: "Invalid Token"},
-        //                     {isSuccessful: "TRUE"},
-        //             {user: null}
+            return  res.json({
+                posts : [
+                    {statusCode: "1005"},
+                            {message: "Invalid Token"},
+                            {isSuccessful: "TRUE"},
+                    {user: null}
         
-        //         ]  
-        //     })
-        
-        
-        // }
-        // var salt = token.split("_");
-        // // 'fHX4xp6L9IE1YUfCLknn9w==','DcV0VRYa3D48MhvAWj1qtxpHt6TtSbcNAbXX5UfhrSg='
+                ]  
+            })
         
         
-        //     if(!(validateKey.validate(salt[0],salt[1]))){
-        //         return  res.json({
-        //             posts : [
-        //                 {statusCode: "1000"},
-        //                 {message: "Unauthorized Request"},
-        //                 {isSuccessful: "TRUE"},
-        //                 {user: null}
+        }
+        var salt = token.split("_");
+        // 'fHX4xp6L9IE1YUfCLknn9w==','DcV0VRYa3D48MhvAWj1qtxpHt6TtSbcNAbXX5UfhrSg='
         
-        //             ]  
-        //         })
-        //     }
+        
+            if(!(validateKey.validate(salt[0],salt[1]))){
+                return  res.json({
+                    posts : [
+                        {statusCode: "1000"},
+                        {message: "Unauthorized Request"},
+                        {isSuccessful: "TRUE"},
+                        {user: null}
+        
+                    ]  
+                })
+            }
         
             setConnectionPool.connect(function (err, client, release) {
                 if (err) {
