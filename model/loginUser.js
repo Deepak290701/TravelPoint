385c1996e7521cb307478ed5730d9a973e30d0bd
vivekkm251 <kalamtechcorp@gmail.com>
2020-07-19 14:14:30 +0530

    Login response Datatype changes

--- model/loginUser.js
+++ model/loginUser.js
@@ -19,42 +19,33 @@ class loginUser {
     if(!(token.length==69)){
     
         return  res.json({
-            posts : [
-                {statusCode: "1005"},
-                        {message: "Invalid Token"},
-                        {isSuccessful: "TRUE"},
-                {user: null}
-    
-            ]  
-        })
-    
-    
+                isSuccessful: true,
+                statusCode: 1005,
+                message: "Invalid Token",
+                user: null
+        });
     }
     
     // 'fHX4xp6L9IE1YUfCLknn9w==','DcV0VRYa3D48MhvAWj1qtxpHt6TtSbcNAbXX5UfhrSg='
     
-            if(!(validateKey.validate(salt[0],salt[1]))){
+            if(!(validateKey.validate(salt[0].concat(" "),salt[1]))){
                 return  res.json({
-                    posts : [
-                        {statusCode: "1000"},
-                        {message: "Unauthorized Request"},
-                        {isSuccessful: "TRUE"},
-                        {user: null}
-        
-                    ]  
+                    
+                        isSuccessful: true,
+                        statusCode: 1000,
+                        message: "Unauthorized Request",
+                        user: null
                 })
             }
     
     
             if (err) {
                 return  res.json({
-                    posts : [
-                        {statusCode: "1004"},
-                        {message: "Something went wrong.Please Try again later"},
-                        {isSuccessful: "TRUE"},
-                        {user: null}
-    
-                    ]  
+                        isSuccessful: true,
+                        statusCode: 1004,
+                        message: "Something went wrong.Please Try again later",
+                        user: null
+     
                 }); // Error in aquiring connection
               }
             else{
@@ -63,13 +54,11 @@ class loginUser {
                 typeof req.body.password =='undefined'  ){
 
                     return  res.json({
-                        posts : [
-                            {statusCode: "1006"},
-                            {message: "Mandatory values missing"},
-                            {isSuccessful: "TRUE"},
-                            {user: null}
-        
-                        ]  
+                      
+                            isSuccessful: true,
+                            statusCode: 1006,
+                            message: "Mandatory values missing",
+                            user: null   
                     }) 
 
                 }
@@ -82,45 +71,43 @@ class loginUser {
                     release();
                     if (err) {
                         return res.json({
-                            posts : [
-                                {statusCode: "1005"},
-                                {message: "Mobile number is not registered.Please click SignUp"},
-                                {isSuccessful: "TRUE"},
-                                {user: null}
-    
-                            ]  
+                                isSuccessful: true,
+                                statusCode: 1005,
+                                message: "Mobile number is not registered.Please click SignUp",
+                                user: null
+     
                         }) // Error in Executing Query
                       }
                       if(results.rowCount==1){
                         res.json({
-                            posts : [
-                                {statusCode: "1001"},
-                                {message: "Login is Successful"},
-                                {isSuccessful: "TRUE"},
-                                {user:
-                                [
-                                {   id: req.body.id,
-                                    phoneNumber : req.body.phoneNumber,
+                            
+                                isSuccessful: true,
+                                statusCode: 1001,
+                                message: "Login is Successful",
+                                user:
+                                
+                                {   
+
                                     name : req.body.name,
+                                    countryCode : req.body.countryCode,
+                                    mobileNo : req.body.phoneNumber,
                                     password : req.body.password,
                                     mobileNo_verified_at: null,
                                     created_at:null,
                                     updated_at:null,
-                                    registration_type:null,
-                                    countryCode : req.body.countryCode
-                                    } 
-                                ]}
-                            ]
+                                    
+                                } 
+                                
+                            
                         })
                       }
                       else{
                         res.json({
-                            posts : [
-                                {statusCode: "1002"},
-                                {message: "Invalid Mobile Number or Password"},
-                                {isSuccessful: "TRUE"},
-                                {user: null}
-                            ]
+                         
+                                isSuccessful: true,
+                                statusCode: 1002,
+                                message: "Invalid Mobile Number or Password",
+                                user: null
                         })  
                       }
                    
