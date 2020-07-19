ab767bd4449f295cae4bf17c09e08f3a070a2e02
vivekkm251 <kalamtechcorp@gmail.com>
2020-07-19 11:48:55 +0530

    Registration response Datatype changes

--- model/registerUser.js
+++ model/registerUser.js
@@ -88,6 +88,23 @@ class registerUser {
                     }
         
         
+                    const insQuery2 = format('SELECT * FROM users WHERE "phoneNumber" = (%L)',
+                    phoneNumber);
+
+                    client.query(insQuery2,(err,results) => {
+                        if(results.rowCount==1){
+                            return res.json({
+                                
+                                isSuccessful: true,
+                                statusCode: 1003,
+                                message: "Mobile Number already registered",
+                                user: null
+                            })
+                        }
+
+                    })
+
+
                     const insQuery = format('INSERT INTO users("phoneNumber",name,password,id,"countryCode") VALUES (%L)',
                     [phoneNumber,name,password,id,countryCode]);
         
@@ -99,8 +116,8 @@ class registerUser {
                             return res.json({
                                 
                                 isSuccessful: true,
-                                statusCode: 1003,
-                                message: "Mobile Number already registered",
+                                statusCode: 1008,
+                                message: "Something went wrong. Try again.",
                                 user: null
         
                                 
